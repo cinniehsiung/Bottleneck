9e7a361b0b10c9ee2bff8d5f8d67276b6563034b
Allen <allenzhu@berkeley.edu>
2020-04-05 12:23:43 -0400

    Implement Soatto's small AlexNet model

--- alexnet.py
+++ alexnet.py
@@ -1,62 +1,36 @@
 # modified from https://github.com/pytorch/vision/blob/master/torchvision/models/alexnet.py
+# based on Soatto disentanglement page 24
+# omitting batch norm for now
 
 import torch
 import torch.nn as nn
-
-
-__all__ = ['AlexNet', 'alexnet']
-
-
-model_urls = {
-    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
-}
-
 
 class AlexNet(nn.Module):
 
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
-            #nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-
+            nn.Conv2d(3, 64, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            #nn.MaxPool2d(kernel_size=3, stride=2),
             nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),
-            #nn.Conv2d(64, 192, kernel_size=5, padding=2),
-
+            nn.Conv2d(64, 64, kernel_size=5, padding=2),
+            # BN
             nn.ReLU(inplace=True),
-            #nn.MaxPool2d(kernel_size=3, stride=2),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-           nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-
-            nn.MaxPool2d(kernel_size=2),
-            #nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 2 * 2, 4096),
+            nn.Linear(7*7*64, 384),
+            # BN
             nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
+            nn.Linear(384, 192),
+            # BN
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
+            nn.Linear(192, 10),
+            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
         x = self.features(x)
-        #x = self.avgpool(x)
-
-        x = x.view(x.size(0), 256*2*2)
-        #x = torch.flatten(x, 1)
-
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x 
