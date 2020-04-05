b749ab5d3a485bc6063c52f6a3027d322cb09b14
Cinnie Hsiung <cynnifer@Cinnies-MacBook-Pro-16.local>
2020-04-05 10:39:27 -0400

    removed extra colon at the end of alexnet

--- alexnet.py
+++ alexnet.py
@@ -134,4 +134,4 @@ def alexnet_v2(inputs,
             net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
           end_points[sc.name + '/fc8'] = net
       return net, end_points
-alexnet_v2.default_image_size = 224:
+alexnet_v2.default_image_size = 224
